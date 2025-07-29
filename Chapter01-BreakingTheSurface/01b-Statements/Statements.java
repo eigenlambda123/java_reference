1e735fae26825941f60882087b3616e8785b8154
eigenlambda123 <rmvilla987@gmail.com>
2025-07-29 08:59:58 +0800

    01b-Statements

---
+++ Chapter01-BreakingTheSurface/01b-Statements/Statements.java
@@ -0,0 +1,20 @@
+public class Statements {
+	public static void main (String[] args) {
+		// integer assignment
+		int x = 3;
+		System.out.print("x is still " + x);
+
+		// string assignment
+		String name = "Dirk";
+		System.out.print("the name is" + name);
+
+		// multiplication
+		x = x * 17;
+		System.out.print("x * 17 is " + x);
+
+		// random number
+		double d = Math.random();
+		System.out.print("Random number: " + d);
+
+	}
+}
