98d26f4907c427252c35af6d7d508232744077c4
eigenlambda123 <rmvilla987@gmail.com>
2025-08-01 22:53:52 +0800

    01d-Exercises

---
+++ Chapter02-ATripToObjectville/01d-Exercises/DrumKit/DrumKitTestDrive.java
@@ -0,0 +1,33 @@
+class DrumKit {
+	
+	boolean topHat = true;
+	boolean snare = true;
+
+	// playSnare method
+	void playSnare() {
+		System.out.println("bang bang ba-bang");
+	}
+
+	// playTopHat methpd
+	void playTopHat() {
+		System.out.println("ding ding da-ding");
+	}
+}
+
+class DrumKitTestDrive {
+	public static void main(String [] args) {
+		
+		
+		DrumKit d = new DrumKit();
+		
+
+		
+		if (d.snare == true) {
+			d.playSnare();
+			d.snare = false;
+		}
+
+		d.playTopHat();
+
+	}
+}
