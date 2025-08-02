ffd54dc054e4889fe353cd701a022b10285986d3
eigenlambda123 <rmvilla987@gmail.com>
2025-08-02 14:52:36 +0800

    03a-PrimitiveVariables

---
+++ Chapter03-KnowYourVariables/03a-PrimitiveVariables/PrimitiveVariables.java
@@ -0,0 +1,28 @@
+public class PrimitiveVariables {
+    public static void main(String[] args) {
+
+	// Small integer (-128 to 127) - 8bit
+        byte b = 100;
+
+	// Larger than byte (-32k to 32k) - 16bit
+	short s = 1000;
+
+	//Standard integer type - 32bit
+	int x = 42; 
+
+	// Big integers - 64bit
+	long l = 900000L;
+
+	// Single-precision decimal - 32bit
+	float f = 3.14f;
+
+	// Double-precision decimal - 64bit
+	double d = 2.718;
+
+	// Single Unicode character - 16bit
+	char c = 'A';
+
+	// Logical value (`true` or `false`) - 1bit
+	boolean fa = false;
+    }
+}
