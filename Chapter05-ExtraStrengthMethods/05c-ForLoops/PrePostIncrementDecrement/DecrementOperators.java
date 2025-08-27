f3e42eebeb1654581348a86e3b5a1fa64d6b520b
eigenlambda123 <rmvilla987@gmail.com>
2025-08-27 14:08:10 +0800

    05c-ForLoops

---
+++ Chapter05-ExtraStrengthMethods/05c-ForLoops/PrePostIncrementDecrement/DecrementOperators.java
@@ -0,0 +1,11 @@
+class DecrementOperators{
+	public static void main(String[] args) {
+		// Pre Decrement
+		int i = 1;
+		System.out.println(--i);
+
+		// Post Decrement
+		int j = 1;
+		System.out.println(j--);
+	}
+}
---
+++ Chapter05-ExtraStrengthMethods/05c-ForLoops/PrePostIncrementDecrement/IncrementOperators.java
@@ -0,0 +1,11 @@
+class IncrementOperators{
+	public static void main(String[] args) {
+		// Pre Increment
+		int i = 1;
+		System.out.println(++i);
+
+		// Post Increment
+		int j = 1;
+		System.out.println(j++);
+	}
+}
